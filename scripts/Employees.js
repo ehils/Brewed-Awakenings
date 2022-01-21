2a6e7e34b53ef902d9968057d68489ddc01b918a
Erich Hils <mrerichhils@gmail.com>
2022-01-21 14:32:38 -0600

    finished brewed awakenings

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,16 +1,52 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
-    let html = "<ul>"
+    let employeeHtml = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employeeId}">${employee.name}</li>`
+        employeeHtml += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
-    html += "</ul>"
+    employeeHtml += "</ul>"
 
-    return htm
+    return employeeHtml
 }
+
+const employeeOrders = (employee) => {
+    let fulfilledOrders = 0
+
+    for (const order of orders) {
+        if (order.employeeId === employee.id) {
+            // Increment the number of fulfilled orders
+            fulfilledOrders++
+        }
+    }
+    return fulfilledOrders// Return how many orders were fulfilled
+}
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        // refers to the Id in each list item
+        if (itemClicked.id.startsWith("employee")) {
+            // productId is var name
+            const [,employeeId] = itemClicked.id.split("--")
+            
+            for (const employee of employees) {
+                // [,employeeId] exists as string, parseInt turns into Int, compares to int of product.id
+                if (employee.id === parseInt(employeeId)) {
+
+                   const orderCount = employeeOrders(employees)
+                
+                    window.alert(`${employee.name} sold ${orderCount} products.`)
+                }
+            }
+        }
+    }
+)
 
--- scripts/Products.js
+++ scripts/Products.js
@@ -3,14 +3,36 @@ import { getProducts } from "./database.js"
 const products = getProducts()
 
 export const Products = () => {
-    let html = "<ul>`
+    let productHtml = "<ul>"
 
     for (const product of products) {
-        html += `<li id="product--${product.id}">${product.title}</li>`
+        // each list item will have unique Id of "erich--(id value of product"
+        productHtml += `<li id="erich--${product.id}">${product.name}</li>`
     }
 
-    html += "</ul>"
+    productHtml+= "</ul>`"
 
-    return html
+    return productHtml
 }
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        // refers to the Id in each list item
+        if (itemClicked.id.startsWith("erich")) {
+            // productId is var name
+            const [,productId] = itemClicked.id.split("--")
+            
+            for (const product of products) {
+                // [,productId] exists as string, parseInt turns into Int, compares to int of product.id
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs $${product.price}.`)
+                }
+            }
+        }
+    }
+)
+            
+            
 
